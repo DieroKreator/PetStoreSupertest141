1232f54fceed4f4c4b85d4ef0191bb495b25b8e5
DieroKreator <qtechwired@gmail.com>
2024-09-29 10:59:36 -0300

    first commit

---
+++ test/api/pet.spec.js
@@ -0,0 +1,20 @@
+// Bibliotecas e Framework
+const supertest = require('supertest')
+
+const petId = 602740501
+
+// Em JavaScript, Classe é opcional
+describe('API PetStore Swagger - Entidade Pet', () => {
+
+    const request = supertest("https://petstore.swagger.io/v2")
+
+    // Funções ou Métodos
+    it('POST Pet', () => {
+        // Atributos, Campos, Caracteristicas, Configuracoes
+    
+        // Funcoes de Apoio (Opcional)
+
+        // Funcoes de Teste em Si
+    })
+    
+})
