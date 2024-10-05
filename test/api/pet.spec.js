d9bf09e58f107504c4641d59b9bc223661652a67
DieroKreator <qtechwired@gmail.com>
2024-10-05 10:58:59 -0300

    metodos post, get, put e delete

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -9,15 +9,60 @@ describe('API PetStore Swagger - Entidade Pet', () => {
     const request = supertest("https://petstore.swagger.io/v2")
 
     // Funções ou Métodos
-    it('POST Pet', () => {
+    it('POST Pet', async () => {
         // Atributos, Campos, Caracteristicas, Configuracoes
-    
-        // Funcoes de Apoio (Opcional)
+        const pet = await require('../../vendors/json/pet.json')
 
         // Funcoes de Teste em Si
+        return await request
+            .post('/pet')
+            .send(pet)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(petId)
+                expect(res.body.name).toBe('Snoopy')
+                expect(res.body.category.name).toBe('dog')
+                expect(res.body.tags[0].name).toBe('vaccinated')
+            })
+
     })
 
-    it('GET Pet', () => {
+    it('GET Pet', async () => {
+
+        return await request
+            // .get('/pet/' + petId') // tradicional
+            .get(`/pet/${petId}`) // moderno: template literals
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(petId)
+                expect(res.body.status).toBe('available')
+            })
+        
+    })
+
+    it('PUT Pet', async () => {
+        // Atributos, Campos, Caracteristicas, Configuracoes
+        const pet = await require('../../vendors/json/petput.json')
+
+        // Funcoes de Teste em Si
+        return await request
+            .put('/pet')
+            .send(pet)
+            .then((res) => {
+                expect(res.statusCode).toEqual(200)
+                expect(res.body.status).toEqual('sold')
+            })
+
+    })
+
+    it('DELETE Pet', async () => {
+
+        return await request
+            .delete(`/pet/${petId}`) // moderno: template literals
+            .then((res) => {
+                expect(res.body.code).toEqual(200)
+                expect(res.body.message).toEqual(petId.toString())
+            })
         
     })
 
