9e2b1498bd2e566e811a2b9c857aa21d913e11c6
DieroKreator <qtechwired@gmail.com>
2024-10-05 18:33:13 -0300

    Teste post com massa de dados

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -7,6 +7,7 @@ const petId = 602740501
 describe('API PetStore Swagger - Entidade Pet', () => {
 
     const request = supertest("https://petstore.swagger.io/v2")
+    const massa1 = require('../../vendors/json/massaPet')
 
     // Funções ou Métodos
     it('POST Pet', async () => {
@@ -26,6 +27,37 @@ describe('API PetStore Swagger - Entidade Pet', () => {
             })
 
     })
+
+    // Método POST que lê e cria 3 registros
+    it.each(massa1.array.map(elemento => [
+        elemento.nomePet,
+        elemento.idPet,
+        elemento.nomeCategoria,
+        elemento.idCategoria
+    ]))
+    ('POST Pet Data Driven Simples: %s', (nomePet, idPet, nomeCategoria, idCategoria) => {
+        // Atributos, Campos, Caracteristicas, Configuracoes
+        const pet = require('../../vendors/json/pet.json')
+
+        // Sustituimos os campos que queremos personalizar através da massa
+        pet.id = idPet
+        pet.name = nomePet
+        pet.category.id = idCategoria
+        pet.category.name = nomeCategoria
+
+        // Funcoes de Teste em Si
+        return request
+            .post('/pet')
+            .send(pet)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(idPet)
+                expect(res.body.name).toBe(nomePet)
+                expect(res.body.category.name).toBe(nomeCategoria)
+                expect(res.body.tags[0].name).toBe('vaccinated')
+            })
+
+    })    
 
     it('GET Pet', async () => {
 
@@ -60,10 +92,15 @@ describe('API PetStore Swagger - Entidade Pet', () => {
         return await request
             .delete(`/pet/${petId}`) // moderno: template literals
             .then((res) => {
+                expect(res.statusCode).toEqual(200)
                 expect(res.body.code).toEqual(200)
                 expect(res.body.message).toEqual(petId.toString())
             })
         
     })
+
+    // Testes Data Driven do CRUD (POST, GET, PUT e Delete)
+
+    
 
 })
---
+++ vendors/json/massaPet.js
@@ -0,0 +1,20 @@
+module.exports.array = [
+    {
+      "nomePet": "Balt",
+      "idPet": 602740502,
+      "nome Categoria": "Dog",
+      "idCategoria": 1
+    },
+    {
+      "nomePet": "Spoky",
+      "idPet": 602740503,
+      "nome Categoria": "Dog",
+      "idCategoria": 1
+    },
+    {
+      "nomePet": "Bichenko",
+      "idPet": 602740504,
+      "nome Categoria": "Cat",
+      "idCategoria": 2
+    }
+]
